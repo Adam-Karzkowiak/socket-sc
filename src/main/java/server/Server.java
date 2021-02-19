084749b92174838cc8692d1c4c03b969e31a010a
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-19 22:10:04 +0100

    This is official init

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,12 +1,18 @@
 package server;
 
-import java.io.IOException;
+
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 class Server {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
 
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(ServerConstants.SERVER_PORT);
+
+
+    public static void main(String[] args) {
     }
 }
--- src/main/java/server/ServerConstants.java
+++ src/main/java/server/ServerConstants.java
@@ -1,5 +1,8 @@
 package server;
 
+import java.time.LocalDateTime;
+
 class ServerConstants {
     static final int SERVER_PORT = 7777;
+    static final LocalDateTime SERVER_LAUNCH= LocalDateTime.now();
 }
