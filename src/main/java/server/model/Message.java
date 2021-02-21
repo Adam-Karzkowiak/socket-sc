8c6ad363316d368d3360c536f79d33b58584b492
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-21 23:12:56 +0100

    Message class fields created

--- src/main/java/server/model/Message.java
+++ src/main/java/server/model/Message.java
@@ -1,4 +1,11 @@
 package server.model;
 
+import javax.validation.constraints.Max;
+import java.time.LocalDateTime;
+
 class Message {
+    String recipient;
+    @Max(255)
+    String message;
+    LocalDateTime sentDate;
 }
