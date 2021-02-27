5fdb01477f54c74ba4b89135a4592dae67af811c
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-27 19:21:30 +0100

    Message model with form validation created

--- src/main/java/server/model/Message.java
+++ src/main/java/server/model/Message.java
@@ -1,11 +1,34 @@
 package server.model;
 
+
+import server.projection.MessageWriteModel;
+
 import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 class Message {
+
+    @NotNull
     String recipient;
+    @Min(1)
     @Max(255)
-    String message;
-    LocalDateTime sentDate;
+    String body;
+    LocalDateTime createdOn;
+
+    private Message(String recipient, String body, LocalDateTime createdOn) {
+        this.recipient = recipient;
+        this.body = body;
+        this.createdOn = createdOn;
+    }
+
+
+    public static Message createMsg(MessageWriteModel entity) {
+        return new Message(
+                entity.getRecipient(),
+                entity.getBody(),
+                LocalDateTime.now()
+        );
+    }
 }
