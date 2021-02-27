425f3264b3503620b41b83103b60da49375cbccb
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-27 22:19:07 +0100

    AppUser class fields created

--- src/main/java/server/model/AppUser.java
+++ src/main/java/server/model/AppUser.java
@@ -1,4 +1,13 @@
 package server.model;
 
+import server.model.utility.Privileges;
+
+import javax.validation.constraints.NotNull;
+
 class AppUser {
+    @NotNull
+    String login;
+    @NotNull
+    String password;
+    Privileges privileges;
 }
